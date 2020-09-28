df240864a877fbcc4337e4dacb6bb1c14da9b7e5
Lukas Schmidt <lightsprint09@users.noreply.github.com>
2020-09-28 14:41:39 +0200

    Update Line.swift

--- Sources/FPTF/Items/Line.swift
+++ Sources/FPTF/Items/Line.swift
@@ -1,12 +1,12 @@
 public struct Line: Item {
     public let type = "line"
 
-    public var id: String
-    public var name: String
+    public var id: String?
+    public var name: String?
     public var mode: Mode
     public var `operator`: Ref<Operator>
 
-    public init(id: String, name: String, mode: Mode, operator: Ref<Operator>) {
+    public init(id: String?, name: String?, mode: Mode, operator: Ref<Operator>) {
         self.id = id
         self.name = name
         self.mode = mode
